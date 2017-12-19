2ae1c945859b737011258b13f35b684a19ec0a4d
leoman730 <leoman730@gmail.com>
2017-12-19 17:27:41 -0500

    Add script for brew.

--- install.sh
+++ install.sh
@@ -17,24 +17,24 @@ source install/link.sh
 
 source install/git.sh
 
-#
-# # only perform macOS-specific install
-# if [ "$(uname)" == "Darwin" ]; then
-#     echo -e "\n\nRunning on OSX"
-#
-#     source install/brew.sh
-#
-#     source install/osx.sh
-#
-#     # create a backup of the original nginx.conf
-#     if [ -f /usr/local/etc/nginx/nginx.conf ]; then
-#         mv /usr/local/etc/nginx/nginx.conf /usr/local/etc/nginx/nginx.conf.original
-#     fi
-#
-#     ln -s ~/.dotfiles/nginx/nginx.conf /usr/local/etc/nginx/nginx.conf
-#     # symlink the code.dev from dotfiles
-#     ln -s ~/.dotfiles/nginx/code.dev /usr/local/etc/nginx/sites-enabled/code.dev
-# fi
+
+# only perform macOS-specific install
+if [ "$(uname)" == "Darwin" ]; then
+    echo -e "\n\nRunning on OSX"
+
+    source install/brew.sh
+
+    # source install/osx.sh
+
+    # create a backup of the original nginx.conf
+    # if [ -f /usr/local/etc/nginx/nginx.conf ]; then
+    #     mv /usr/local/etc/nginx/nginx.conf /usr/local/etc/nginx/nginx.conf.original
+    # fi
+    #
+    # ln -s ~/.dotfiles/nginx/nginx.conf /usr/local/etc/nginx/nginx.conf
+    # # symlink the code.dev from dotfiles
+    # ln -s ~/.dotfiles/nginx/code.dev /usr/local/etc/nginx/sites-enabled/code.dev
+fi
 #
 # echo "creating vim directories"
 # mkdir -p ~/.vim-tmp
---
+++ install/brew.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+
+if test ! $(which brew); then
+    echo "Installing homebrew"
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+
+echo -e "\n\nMake sure we’re using the latest Homebrew"
+echo "=============================="
+brew update
+
+echo -e "\n\nUpgrade any already-installed formulae"
+echo "=============================="
+brew upgrade
+
+
+
+echo -e "\n\nInstalling homebrew packages..."
+echo "=============================="
+
+formulas=(
+    # flags should pass through the the `brew list check`
+    'macvim --with-override-system-vim'
+    ack
+    diff-so-fancy #make your diff's human readable instead of machine readable.
+    dnsmasq # local dns
+    fzf
+    git
+    'grep --with-default-names'
+    highlight
+    hub # better git
+    markdown
+    neovim/neovim/neovim  # better vim
+    node
+    # nginx
+    reattach-to-user-namespace  #  pbpaste and pbcopy under tmux
+    the_silver_searcher # better search than grep, ack
+    tmux
+    tree  # list file in tree format
+    'wget --enable-iri' #Install wget with IRI support
+    z
+    zsh
+    ripgrep # better search for developer
+    git-standup # See user commits log by day range
+    entr  # Run arbitrary commands when files change
+    coreutils # Install GNU core utilities (those that come with OS X are outdated)
+    findutils # Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
+)
+
+for formula in "${formulas[@]}"; do
+    if brew list "$formula" > /dev/null 2>&1; then
+        echo "$formula already installed... skipping."
+    else
+        brew install $formula
+    fi
+done
+
+# Remove outdated versions from the cellar
+brew cleanup
+
+
+echo "Add $(brew --prefix coreutils)/libexec/gnubin to \$PATH."
+PATH=$PATH:$(brew --prefix coreutils)/libexec/gnubin
+export PATH
