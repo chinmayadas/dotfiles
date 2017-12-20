4d566c9280f45276ed4ce5899faaee8a4cd235f6
leoman730 <leoman730@gmail.com>
2017-12-20 16:12:11 -0500

    Add more brew formula

--- install/brew.sh
+++ install/brew.sh
@@ -41,11 +41,17 @@ formulas=(
     'wget --enable-iri' #Install wget with IRI support
     z
     zsh
+    fish  # fish shell
     ripgrep # better search for developer
     git-standup # See user commits log by day range
     entr  # Run arbitrary commands when files change
     coreutils # Install GNU core utilities (those that come with OS X are outdated)
     findutils # Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
+    lynx  # textbase browser
+    rename  # Perl-powered file rename script with many helpful built-ins
+    sqlmap  # Penetration testing for SQL injection and database servers
+    'gnu-sed --with-default-names'  # Install GNU `sed`, overwriting the built-in `sed`.
+
 )
 
 for formula in "${formulas[@]}"; do
