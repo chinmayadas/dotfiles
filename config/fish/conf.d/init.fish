14a81e74e7375b67cd0f553c7e0deacce8bc77d8
leoman730 <leoman730@gmail.com>
2018-01-18 16:05:34 -0500

    Set up variable.sh and some clean up for fish shell.

--- config/fish/conf.d/init.fish
+++ config/fish/conf.d/init.fish
@@ -2,3 +2,5 @@ set -x FISH_CONF $HOME/.config/fish/conf.d
 
 source $FISH_CONF/variables.fish
 source $FISH_CONF/alias.fish
+
+echo "in $FISH_CONF/init.fish"
--- config/fish/conf.d/variables.fish
+++ config/fish/conf.d/variables.fish
@@ -1,1 +1,11 @@
-set -xg CODE_DIR "$HOME/git_repos"
+# set variables
+set -g -x EDITOR vim
+
+set -gx PATH /usr/local/bin $PATH
+
+set -gx CODE ~/git_repos
+
+set -gx svnlive $CODE/its/live
+set -gx svntest $CODE/its/test
+set -gx svndev $CODE/its/dev
+
